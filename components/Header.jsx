779913cb7c20b5b8b71d2613c90a892f03693b72
Ghullhamer <tron15963@gmail.com>
2025-05-02 01:24:18 +0300

    Header component

---
+++ components/Header.jsx
@@ -0,0 +1,23 @@
+
+
+export default function Header(){
+
+    return(
+        <div className="bg-white w-full h-21 flex justify-between px-25 ">
+            <div className="flex font-[family-name:var(--font-monomaniac-one)] text-6xl h-full  items-center gap-8">
+                <a className="h-15 w-15 bg-slate-400 self-alight-center"></a>
+                <a className="pb-2">Yury</a>
+                
+                
+            </div>
+            <ul className="flex gap-10 text-2xl font-[family-name:var(--font-martian-mono)] h-full items-center">
+                <li>Головна</li>
+                <li>Про</li>
+                <li>Досвід</li>
+                <li>Контакти</li>
+            </ul>
+        
+        </div>
+    )
+
+}
